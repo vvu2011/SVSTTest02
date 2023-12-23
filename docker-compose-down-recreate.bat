6c323858785fa7a2d75c553428941a0d258458c4
proglamer <vvu2011@gmali.com>
2023-12-23 14:29:40 +0300

    правка батника рестарта контейнеров

--- docker-compose-down-recreate.bat
+++ docker-compose-down-recreate.bat
@@ -1,2 +1,5 @@
 docker-compose --project-name svsttest02 down
+rem docker system prune -a
+docker rmi svsttest02
+docker rmi svsttest002client
 docker-compose --project-name svsttest02 up --force-recreate -d
