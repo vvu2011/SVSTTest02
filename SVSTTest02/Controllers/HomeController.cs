b90477e412cdadaefa3fc0d7e368706d977ceec5
proglamer <vvu2011@gmali.com>
2024-01-04 21:10:39 +0300

    глубину выборки архивных данных для диаграммы изменил с пяти минут на одну, тиак как для пяти минут получяается слишком много данных

--- SVSTTest02/Controllers/HomeController.cs
+++ SVSTTest02/Controllers/HomeController.cs
@@ -67,7 +67,7 @@ namespace SVSTTest02.Controllers
             string json = "ok";
 
             // запрос данных из таблицы
-            DateTime begin = DateTime.UtcNow.AddMinutes(-5);
+            DateTime begin = DateTime.UtcNow.AddMinutes(-1);
             List<GAS_VALUESModel> valsUtcTime = _context.GAS_VALUES
                 .Where(s => s.GAS_VAL_DATE > begin)
                 .ToList();
